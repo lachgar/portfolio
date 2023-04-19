737631564b8fbca251ab880dab466cb6d57acee7
errattahi <errattahi.r@ucd.ac.ma>
2023-04-19 18:44:31 +0000

    'init'

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,48 +1,18 @@
+// ProfileImage.js
+
 import React from "react";
-import PropTypes from "prop-types";
-import photo from "../assets/moi.jpg";
+import photo from"../assets/moi.jpg";
 
-function ProfileImage({ size, shape, className, alt, name, socialProfile, showName, showSocialProfile }) {
-
-  const handleClick = () => {
-    // Ajouter une fonctionnalité pour changer la photo ici
-  }
-
+function ProfileImage() {
   return (
-    <div className={`text-center mb-4 ${className}`}>
+    <div className="text-center mb-4">
       <img
-        src={photo}
-        alt={alt}
-        className={`rounded-${shape} profile-img`}
-        style={{width: size, height: size, cursor: "pointer"}}
-        onClick={handleClick}
+        src={photo} // Remplacez par le chemin de votre propre photo de profil
+        alt="photo"
+        className="rounded-circle profile-img"
       />
-      {showName && <div className="mt-2">{name}</div>}
-      {showSocialProfile && <div className="mt-2">{socialProfile}</div>}
     </div>
   );
-}
-
-ProfileImage.defaultProps = {
-  size: "150px",
-  shape: "circle",
-  className: "",
-  alt: "profile photo",
-  name: "",
-  socialProfile: "",
-  showName: true,
-  showSocialProfile: false
-}
-
-ProfileImage.propTypes = {
-  size: PropTypes.string,
-  shape: PropTypes.oneOf(["circle", "square"]),
-  className: PropTypes.string,
-  alt: PropTypes.string,
-  name: PropTypes.string,
-  socialProfile: PropTypes.string,
-  showName: PropTypes.bool,
-  showSocialProfile: PropTypes.bool
 }
 
 export default ProfileImage;
